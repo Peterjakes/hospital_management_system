3d5b2b1a80f0d8e42ae63e2686c17d43b161efb6
Peter Jakes <kiberujakes@gmail.com>
2025-09-16 08:02:00 +0300

    added basic Department class with essential fields

--- lib/models/department_model.dart
+++ lib/models/department_model.dart
@@ -1,1 +1,16 @@
-// TODO Implement this library.
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+// Department model representing hospital departments
+class Department {
+  final String id;
+  final String name;
+  final String description;
+  final String headDoctorId;
+
+  Department({
+    required this.id,
+    required this.name,
+    required this.description,
+    required this.headDoctorId,
+  });
+}
