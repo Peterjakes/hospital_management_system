7c19e38523052280b5d394aac46ec270be4f82e4
Peter Jakes <kiberujakes@gmail.com>
2025-09-18 20:49:03 +0300

    added DoctorPatientsScreen with search bar

---
+++ lib/screens/doctor/doctor_patients_screen.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+
+/// Doctor patients screen showing all patients
+class DoctorPatientsScreen extends StatefulWidget {
+  const DoctorPatientsScreen({super.key});
+
+  @override
+  State<DoctorPatientsScreen> createState() => _DoctorPatientsScreenState();
+}
+
+class _DoctorPatientsScreenState extends State<DoctorPatientsScreen> {
+  final _searchController = TextEditingController();
+
+  @override
+  void dispose() {
+    _searchController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        _buildSearchBar(),
+        const Expanded(
+          child: Center(
+            child: Text('Patients list will appear here'),
+          ),
+        ),
+      ],
+    );
+  }
+
+  Widget _buildSearchBar() {
+    return Container(
+      padding: const EdgeInsets.all(16),
+      child: TextField(
+        controller: _searchController,
+        decoration: InputDecoration(
+          hintText: 'Search patients...',
+          prefixIcon: const Icon(Icons.search),
+          border: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(12),
+          ),
+        ),
+      ),
+    );
+  }
+}
